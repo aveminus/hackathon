445c072a2e5550c21a4d8781918a0e3b74987970
Kiko Fernandez Reyes <kiko.fernandez@it.uu.se>
2016-10-21 14:53:44 +0200

    templates folder

--- acmuu/views.py
+++ acmuu/views.py
@@ -4,19 +4,21 @@ from .models import Repo
 from django.shortcuts import get_object_or_404
 
 def index(request):
+
     return HttpResponse("Index.")
 
 def detail(request, username, repo):
     repo = get_object_or_404(Repo, path="%s/%s" % (username, repo))
-    return HttpResponse("You're looking at user: %s, %s." % (username, repo))
+    return HttpResponse("You're looking at user: %s, %s." % (username, repo.path))
 
 def user(request, username):
     print username
     return HttpResponse("You're looking at user: %s." % (username))
 
-# def events(request, repo_name):
-#     response = "You're looking at the results of question %s."
-#     return HttpResponse(response % repo_name)
+def events(request, repo_name):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % repo_name)
+
 #
 # def commits(request, repo_id):
 #     return HttpResponse("You're voting on question %s." % repo_id)
